12ea452e20224d8d9b4d3724e8710e42898086d2
anshulsre8171 <anshulsre8171@gmail.com>
2025-04-09 18:52:15 +0530

    ReduxToolkit install

---
+++ src/Redux/slices/authSlice.ts
@@ -0,0 +1,18 @@
+import { createSlice } from "@reduxjs/toolkit";
+// Initial state for authentication 
+const initialState= null
+const authSlice = createSlice({
+    name: "auth",
+    initialState,
+    reducers: {
+        login: (state, action) => { 
+            console.log(action,"yup0");
+            
+            return action.payload
+        },
+        logout: (state) => null
+    },
+});
+// Export actions 
+export const { login, logout } = authSlice.actions;
+export default authSlice.reducer; 
--- src/services/index.ts
+++ src/services/index.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
 const BASEURL = `http://localhost:9000`
 
+
 export const userAuthRegister = async (payload: any) => {
     const response = await axios.post(`${BASEURL}/api/register`, payload);
     return response?.data
 }
-
