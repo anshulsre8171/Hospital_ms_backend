3101c780c9eedaa3aef24c9db597c0abd45282c5
anshulsre8171 <anshulsre8171@gmail.com>
2025-04-09 14:58:22 +0530

    Create  Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,27 @@
+import AwardSection from "@/Components/landingPage/AwardSection"
+import Booking from "@/Components/landingPage/Booking"
+import Carousel from "@/Components/landingPage/Carousel"
+import Cout from "@/Components/landingPage/Cout"
+import FeedbackCarousel from "@/Components/landingPage/FeedbackCarousel"
+import Partners from "@/Components/landingPage/Partners"
+import PersonalCare from "@/Components/landingPage/PersonalCare"
+import Services from "@/Components/landingPage/Services";
+
+import Navbar from "@/Components/landingPage/Navbar";
+import FooterSection from "@/Components/landingPage/FooterSection"
 export default function Home() {
   return (
     <>
-<h1>create project</h1>
+      <Navbar />
+      <Carousel />
+      <PersonalCare />
+      <Cout />
+      <AwardSection />
+      <Services />
+      <Booking />
+      <FeedbackCarousel />
+      <Partners /> 
+      <FooterSection />
     </>
   )
 }
