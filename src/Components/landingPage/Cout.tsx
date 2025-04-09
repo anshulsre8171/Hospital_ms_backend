e9c74bb34c6b5cfb430062262f90e5ea296354d6
anshulsre8171 <anshulsre8171@gmail.com>
2025-04-09 15:21:10 +0530

    CountUp library add

--- src/Components/landingPage/Cout.tsx
+++ src/Components/landingPage/Cout.tsx
@@ -5,59 +5,82 @@ import { FaUserDoctor } from "react-icons/fa6";
 import { FaFlag } from "react-icons/fa";
 import { SlBadge } from "react-icons/sl";
 import { IoIosGlobe } from "react-icons/io";
+import CountUp from 'react-countup';
 const Cout = () => {
   return (
     <>
-     <div className="row mt-5 py-4">
+      <div className="row mt-5 py-4">
         <div className="col-md-10 mx-auto">
-        <div className="row" >
-        <div className="col-lg-3 p-0 col-md-6 col-sm-6">
-          <div className='personal-care'>
-            <div className="ic">
-              <FaUserDoctor />
+          <div className="row" >
+            <div className="col-lg-3 p-0 col-md-6 col-sm-6">
+              <div className='personal-care'>
+                <div className="ic">
+                  <FaUserDoctor />
+                </div>
+                <div className='overlay'>
+                  <span>
+                    <CountUp
+                      start={0}
+                      end={58}
+                      duration={3.75}
+                      />
+                </span>K
+                  <p className=''>Happy People</p>
+                </div>
+              </div>
             </div>
-            <div className='overlay'>
-              <span>58</span>K
-              <p className=''>Happy People</p>
+            <div className="col-lg-3 p-0 col-md-6 col-sm-6 bg-warning">
+              <div className='personal-care'>
+                <div className="ic">
+                  <FaFlag />
+                </div>
+                <div className='overlay'>
+                  <span>            
+                    <CountUp
+                      start={0}
+                      end={700}
+                      duration={3.75}
+                      />+</span>
+                  <p className=''>Surgery Comepleted</p>
+                </div>
+              </div>
             </div>
-          </div>
-        </div>
-        <div className="col-lg-3 p-0 col-md-6 col-sm-6 bg-warning">
-        <div className='personal-care'>
-            <div className="ic">
-              <FaFlag />
+            <div className="col-lg-3 p-0 col-md-6 col-sm-6 bg-info">
+              <div className='personal-care'>
+                <div className="ic">
+                  <SlBadge />
+                </div>
+                <div className='overlay'>
+                  <span>
+                  <CountUp
+                      start={0}
+                      end={40}
+                      duration={3.75}
+                      />+
+                      </span>
+                  <p className=''>Expert Doctors</p>
+                </div>
+              </div>
             </div>
-            <div className='overlay'>
-              <span>700</span>+
-              <p className=''>Surgery Comepleted</p>
-            </div>
-          </div>
-        </div>
-        <div className="col-lg-3 p-0 col-md-6 col-sm-6 bg-info">
-        <div className='personal-care'>
-            <div className="ic">
-              <SlBadge />
-            </div>
-            <div className='overlay'>
-              <span>40</span>+
-              <p className=''>Expert Doctors</p>
-            </div>
-          </div>
-        </div>
-        <div className="col-lg-3 p-0 col-md-6 col-sm-6 bg-warning">
-        <div className='personal-care'>
-            <div className="ic">
-              <IoIosGlobe />
-            </div>
-            <div className='overlay'>
-              <span>20</span>+
-              <p className=''>Worldwide Branch</p>
+            <div className="col-lg-3 p-0 col-md-6 col-sm-6 bg-warning">
+              <div className='personal-care'>
+                <div className="ic">
+                  <IoIosGlobe />
+                </div>
+                <div className='overlay'>
+                  <span>
+                  <CountUp
+                      start={0}
+                      end={20}
+                      duration={3.75}
+                      />+</span>
+                  <p className=''>Worldwide Branch</p>
+                </div>
+              </div>
             </div>
           </div>
         </div>
       </div>
-        </div>
-      </div> 
     </>
   )
 }
