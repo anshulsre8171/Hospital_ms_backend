11e4add24e852bdb3a08bcb2d014f93ead4dfa15
anshulsre8171 <anshulsre8171@gmail.com>
2025-04-12 15:57:13 +0530

    Higher order component

---
+++ src/HOC/DoctorWrap.tsx
@@ -0,0 +1,15 @@
+import { userSession } from "@/helpers/userSession"
+import { useRouter } from "next/navigation";
+import { useEffect } from "react" 
+export function DoctorWrap(WarperComponent: any) {
+    return (props: any) => {  
+        const router=useRouter()
+       const data= userSession(); 
+        useEffect(()=>{
+      if(!(data?.jwtToken && data?.userType=="doctor")){
+        router.push('/login')
+      } 
+        },[])
+        return <WarperComponent { ...props } />
+    }
+}
